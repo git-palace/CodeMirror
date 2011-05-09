codemirror/codemirror5

commit 8b76140aaea3cd1f076196f93449d4462a70efc4

Use a simpler method for filtering out scrollbar clicks

lib/codemirror.js
@@ -1103,18 +1103,15 @@ var CodeMirror = (function() {
     function paddingLeft() {return lineSpace.offsetLeft;}
 
     function posFromMouse(e, liberal) {
-      var off = eltOffset(lineSpace),
-          x = e.pageX() - off.left,
-          y = e.pageY() - off.top;
+      var offW = eltOffset(wrapper), x = e.pageX(), y = e.pageY();
       // This is a mess of a heuristic to try and determine whether a
       // scroll-bar was clicked or not, and to return null if one was
       // (and !liberal).
-      if (!liberal && e.target() != lineSpace.parentNode &&
-          !(e.target() == wrapper && y > (lines.length * lineHeight()) && wrapper.clientHeight > code.offsetHeight))
-        for (var n = e.target(); n != lineDiv && n != cursor; n = n.parentNode)
-          if (!n || n == wrapper) return null;
-      var line = showingFrom + Math.floor(y / lineHeight());
-      return clipPos({line: line, ch: charFromX(clipLine(line), x)});
+      if (!liberal && (x - offW.left > wrapper.clientWidth || y - offW.top > wrapper.clientHeight))
+        return null;
+      var offL = eltOffset(lineSpace);
+      var line = showingFrom + Math.floor((y - offL.top) / lineHeight());
+      return clipPos({line: line, ch: charFromX(clipLine(line), x - offL.left)});
     }
     function onContextMenu(e) {
       var pos = posFromMouse(e);
@@ -1871,7 +1868,7 @@ var CodeMirror = (function() {
 
   // Find the position of an element by following the offsetParent chain.
   function eltOffset(node) {
-    var x = 0, y = 0, n2 = node;
+    var x = 0, y = 0;
     for (var n = node; n; n = n.offsetParent) {x += n.offsetLeft; y += n.offsetTop;}
     for (var n = node; n != document.body; n = n.parentNode) {x -= n.scrollLeft; y -= n.scrollTop;}
     return {left: x, top: y};

git-palace/CodeMirror

commit 3e2c89529276c2d86b65c14b07e4e1118a60eda5

Fix bug introduced in 8b76140aaea3cd1f

lib/codemirror.js
@@ -1870,7 +1870,7 @@ var CodeMirror = (function() {
   function eltOffset(node) {
     var x = 0, y = 0;
     for (var n = node; n; n = n.offsetParent) {x += n.offsetLeft; y += n.offsetTop;}
-    for (var n = node; n != document.body; n = n.parentNode) {x -= n.scrollLeft; y -= n.scrollTop;}
+    for (var n = node.parentNode; n != document.body; n = n.parentNode) {x -= n.scrollLeft; y -= n.scrollTop;}
     return {left: x, top: y};
   }
   // Get a node's text content.
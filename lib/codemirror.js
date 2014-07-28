codemirror/codemirror5

commit 2f14f52e24b35323081878d598465f390db1e955

Fix problem caused by ordering of scroll width measurement and width adjustment
Closes #2730

lib/codemirror.js
@@ -2032,16 +2032,19 @@
     var cm = op.cm, display = cm.display;
     if (op.updatedDisplay) updateHeightsInViewport(cm);
 
+    op.barMeasure = measureForScrollbars(cm);
+
     // If the max line changed since it was last measured, measure it,
     // and ensure the document's width matches it.
     // updateDisplay_W2 will use these properties to do the actual resizing
     if (display.maxLineChanged && !cm.options.lineWrapping) {
-      op.adjustWidthTo = measureChar(cm, display.maxLine, display.maxLine.text.length).left;
+      op.adjustWidthTo = measureChar(cm, display.maxLine, display.maxLine.text.length).left + 3;
       op.maxScrollLeft = Math.max(0, display.sizer.offsetLeft + op.adjustWidthTo +
                                   scrollerCutOff - display.scroller.clientWidth);
+      var wDiff = op.adjustWidthTo - parseInt(display.sizer.style.minWidth || "0");
+      op.barMeasure.scrollWidth += wDiff;
     }
 
-    op.barMeasure = measureForScrollbars(cm);
     if (op.updatedDisplay || op.selectionChanged)
       op.newSelectionNodes = drawSelection(cm);
   }

git-palace/CodeMirror

commit 2c8a181eb49f35e238d8b738ff3c4d004617d0ca

Fix initialization problem introduced by 2f14f52e24b35

lib/codemirror.js
@@ -2041,8 +2041,9 @@
       op.adjustWidthTo = measureChar(cm, display.maxLine, display.maxLine.text.length).left + 3;
       op.maxScrollLeft = Math.max(0, display.sizer.offsetLeft + op.adjustWidthTo +
                                   scrollerCutOff - display.scroller.clientWidth);
-      var wDiff = op.adjustWidthTo - parseInt(display.sizer.style.minWidth || "0");
-      op.barMeasure.scrollWidth += wDiff;
+      var prevMinWidth = display.sizer.style.minWidth;
+      if (prevMinWidth)
+        op.barMeasure.scrollWidth += op.adjustWidthTo - parseInt(prevMinWidth);
     }
 
     if (op.updatedDisplay || op.selectionChanged)